d458d53bcb879f39a156940a2efed15659fbcccd
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-09-27 11:49:42 +0700

    Update __main__.py

--- ubot/__main__.py
+++ ubot/__main__.py
@@ -25,12 +25,7 @@ async def start_ubot(user_id, _ubot):
     ubot_ = Ubot(**_ubot)
     try:
         await asyncio.wait_for(ubot_.start(), timeout=30)
-        await ubot_.join_chat("kynansupport")
-        await ubot_.join_chat("UputtSupport")
-        await ubot_.join_chat("Flukosaa")
-        await ubot_.join_chat("PesulapTelegram")
-        await ubot_.join_chat("amneseey0u")
-        await ubot_.join_chat("KazuSupportGrp")
+
     except RPCError:
         #await remove_ubot(user_id)
         #await rm_all(user_id)
