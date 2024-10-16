457be5a36cc625a14d79496a1e37c0610575b203
antoo69 <antobarry69@gmail.com>
2024-10-16 21:18:50 +0700

    Update config.py

--- ubot/config.py
+++ ubot/config.py
@@ -7,35 +7,35 @@ load_dotenv()
 
 
 DEVS = [
-    2025873184 #DEVAIU
+    6334438071 #DEVAIU
 ]
 
-KYNAN = list(map(int, os.getenv("KYNAN", "2025873184").split()))
+KYNAN = list(map(int, os.getenv("KYNAN", "6334438071").split()))
 
-API_ID = int(os.environ.get("API_ID", "27284509"))
+API_ID = int(os.environ.get("API_ID", "28524972"))
 
-API_HASH = os.environ.get("API_HASH", "dd0e75ac2dda74d3cbe81770b2c05f93")
+API_HASH = os.environ.get("API_HASH", "409130d1d167a2c26862e65e8779a3d6")
 
-LOG_SELLER = int(os.getenv("LOG_SELLER", "-1002291431932"))
+LOG_SELLER = int(os.getenv("LOG_SELLER", "-1002333374162"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001876092598 -1001864253073 -1001451642443 -1001825363971 -1001797285258 -1001927904459 -1001287188817 -1001812143750 -1001608701614 -1001473548283 -1001608847572 -1001982790377 -1001538826310 -1001861414061 -1001876092598").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001876092598 -1001864253073 -1001451642443 -1001825363971 -1001797285258 -1001927904459 -1001287188817 -1001812143750 -1001608701614 -1001473548283 -1001608847572 -1001982790377 -1001538826310 -1001861414061 -1001876092598 -1002079962899 -10015748196677").split()))
 
-USER_ID = list(map(int, os.getenv("USER_ID", "2025873184").split()))
+USER_ID = list(map(int, os.getenv("USER_ID", "6334438071").split()))
 
 OPENAI_KEY = os.getenv(
     "OPENAI_KEY",
-    "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 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 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 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 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 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",
+    "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",
 ).split()
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6131371833:AAEntqgIfw3Zfhvf1akn0Pm7fdclJqdVjM4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7696179651:AAHKlvY1xYz_F14QPv3DcVqJYgBcWUj4MTQ")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "6096141441"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6334438071"))
 
 MAX_BOT = int(os.environ.get("MAX_BOT", "45"))
 
-SKY = int(os.environ.get("SKY", "-1002432688614"))
+SKY = int(os.environ.get("SKY", "-1002479656527"))
 
 MONGO_URL = os.environ.get(
     "MONGO_URL",
-    "mongodb+srv://aknfb45:galehp@cluster0.svvhi22.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://musicbot9809:buburayam1@cluster0.vwbhb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 )
